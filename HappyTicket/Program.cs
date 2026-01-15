901856404990c7f89440a87b345a9f9bd06eb2ef
Elena Kulikova <71148521+ElenaKulikova80@users.noreply.github.com>
2026-01-15 10:07:25 +0300

    Add files via upload

---
+++ HappyTicket/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HappyTicket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solver solver = new Solver();
+            Test test = new Test(solver.Run);
+            test.Run();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HappyTicket/Solver.cs
@@ -0,0 +1,42 @@
+﻿namespace HappyTicket
+{
+    internal class Solver
+    {
+        public string Run(string[] args)
+        {
+            int n = int.Parse(args[0]);
+            return Tickets(n).ToString();
+        }
+
+        private long Tickets(int n)
+        {
+            int maxSum = 9 * n;
+            long[] dp = new long[maxSum + 1];
+            dp[0] = 1; 
+
+            for (int i = 0; i < n; i++)
+            {
+                long[] newDp = new long[maxSum + 1];
+                for (int sum = 0; sum <= maxSum; sum++)
+                {
+                    if (dp[sum] == 0) continue;
+                    for (int digit = 0; digit <= 9; digit++)
+                    {
+                        if (sum + digit <= maxSum)
+                        {
+                            newDp[sum + digit] += dp[sum];
+                        }
+                    }
+                }
+                dp = newDp;
+            }
+            long total = 0;
+            foreach (long count in dp)
+            {
+                total += count * count;
+            }
+
+            return total;
+        }
+    }
+}
